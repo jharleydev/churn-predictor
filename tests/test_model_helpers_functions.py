77713ec033171b7a370bb03d42f12c6cbe216ca8
jharleydev <jharleydev@gmail.com>
2022-01-27 19:30:17 +0000

    update dir tree

---
+++ tests/test_model_helpers_functions.py
@@ -0,0 +1,13 @@
+import pytest
+
+class TestEDA:
+        
+    def test_needsfiles(tmpdir):
+        print(tmpdir)
+        assert 0
+
+    def test_univariate_analysis(tmpdir): 
+        assert 0
+        
+
+
